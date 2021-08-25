b636b3de5ee8295e0ea388331fe7a2cf3f5acf4c
Semih <31005189+sualtindakiadam@users.noreply.github.com>
2021-08-25 16:13:43 +0300

    MachineLearningImageRecognitionApp added

---
+++ MachineLearningImageRecognitionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognitionApp
+//
+//  Created by Semih Kalaycı on 25.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
